3d054caa90a2afef6333584db6c1e649806fd209
WalkFen24 <f.d.walker@outlook.com>
2023-05-19 08:13:34 -0700

    initial commit attempt 2

---
+++ src/main/java/com/example/project/HelloApplication.java
@@ -0,0 +1,48 @@
+package com.example.project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        //loads stage and first scene
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage.setTitle("Year End Project");
+        stage.setScene(new Scene(root, 600, 400));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
+
+
+/*
+//OTHER WAY TO DO IT:
+
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/project/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+ */
